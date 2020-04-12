2a61150ccb6873d27887a3bc93542e9f810d9a92
Indraa145 <34057268+Indraa145@users.noreply.github.com>
2020-04-13 04:55:35 +0900

    Add files via upload

--- ANN_Boston_Rneuralnet.R
+++ ANN_Boston_Rneuralnet.R
@@ -1,3 +1,7 @@
+library(neuralnet)
+library(MASS)
+library(caTools)
+
 dt <- setData(Boston, "medv")
 
 Boston.scaled <- as.data.frame(scale(Boston))
@@ -9,6 +13,7 @@ Boston.scaled$medv <- scale(Boston$medv
                             , scale = max.medv - min.medv)
 
 # Train-test split
+Boston.split <- sample.split(Boston$medv, SplitRatio = 0.75)
 Boston.train.scaled <- Boston.scaled[Boston.split, ]
 Boston.test.scaled <- Boston.scaled[!Boston.split, ]
 
@@ -28,3 +33,15 @@ Boston.nn.8 <- neuralnet(Boston.nn.fmla
                          , data=Boston.train.scaled
                          , hidden=8
                          , linear.output=TRUE)
+
+# Predict - remember the output is scaled
+Boston.5.3.preds.scaled <- neuralnet::compute(Boston.nn.5.3
+                                              , Boston.test.scaled[,1:13])
+Boston.8.preds.scaled <- neuralnet::compute(Boston.nn.8
+                                            , Boston.test.scaled[,1:13])
+
+Boston.5.3.preds <- Boston.5.3.preds.scaled$net.result
+Boston.8.preds <- Boston.8.preds.scaled$net.result
+
+cor(Boston.5.3.preds, Boston.test.scaled$medv)
+cor(Boston.8.preds, Boston.test.scaled$medv)
